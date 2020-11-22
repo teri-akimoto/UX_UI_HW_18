8244a10db1273e435759293d5793f28b58d98171
teri-akimoto <nguyenhlan96@gmail.com>
2020-11-22 01:14:55 -0800

    Scroll Down to Tab Fixed + RWD fixes
    fixes to the fixed button in mobile view + working side tab (shrink/expand) in mobile view.

---
+++ JavaScript/About.js
@@ -0,0 +1,45 @@
+$("#summary-tab").click(function(e) {
+    e.preventDefault();
+    var offset = 0;
+    var target = this.hash;
+    if ($(this).data('offset') != undefined) offset = $(this).data('offset');
+    $('html,body').stop().animate({
+        'scrollTop': $(target).offset().top - offset
+    },'slow', function() {});
+});
+
+$("#ux-skills-tab").click(function(e) {
+    e.preventDefault();
+    var offset = 0;
+    var target = this.hash;
+    if ($(this).data('offset') != undefined) offset = $(this).data('offset');
+    $('html,body').stop().animate({
+        'scrollTop': $(target).offset().top - offset
+    },'slow', function() {});
+});
+
+$("#tool-set-tab").click(function(e) {
+    e.preventDefault();
+    var offset = 0;
+    var target = this.hash;
+    if ($(this).data('offset') != undefined) offset = $(this).data('offset');
+    $('html,body').stop().animate({
+        'scrollTop': $(target).offset().top - offset
+    },'slow', function() {});
+});
+
+$(".side-menu").mousemove(function() {
+    if ($(".side-menu").height() > 20) {
+        console.log("shrink!");
+        $(this).animate({height: '20'}, 300);
+        $(".side-tab").hide(200);
+    }
+
+    else if ($(".side-menu").height() == 20){
+        console.log('show');
+        var menuHeight = window.innerHeight / 6;
+        var resumeLoc = window.innerHeight / 6 + 70 + 20;
+        $(this).animate({height: menuHeight}, 300);
+        $(".side-tab").show(400);
+    }
+});
--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,13 +1,49 @@
-$(".down-hotspot").click(function() {
-    var offset = 160;
-    var target = $("#case-study1");
-    $('html, body').animate({
-        scrollTop: target.offset().top + offset
-    }, 1000);
-});
 
-$(".russian").click(function() {
-    $(".greeting").text('Здравстуйте, я Лана.');
-    $(".russian").cssChange("border", "2px solid black")
-    document.getElementsByClassName('english').style.backgroundImage="url(../icons/emojione_flag-for-united-kingdom-disabled.svg)";
+$(".russian-disabled, .russian").click(function() {
+    console.log("russian");
+    $(".greeting").text("Здравстуйте, я Лана.");
+    $("#statement").text("Дизайнер & иллюстратор.");
+    $("#end-statement").text("Трёхъязычная с обучением в сферах образования и психологии.");
+    $(this).addClass("russian");
+    $(this).removeClass("russian-disabled");
+    $(".english, .english-disabled").addClass("english-disabled");
+    $(".english, .english-disabled").removeClass("english");
+    $(".vietnam, .vietnam-disabled").addClass("vietnam-disabled");
+    $(".vietnam, .vietnam-disabled").removeClass("vietnam");
+})
+
+$(".english-disabled, .english").click(function() {
+        console.log("english");
+        $(".greeting").text("Hello, my name is Lan.");
+        $("#statement").text("Designer & illustrator.");
+        $("#end-statement").text("Trilingual with education and psychology background.");
+        $(this).addClass("english");
+        $(this).removeClass("english-disabled");
+        $(".vietnam, .vietnam-disabled").addClass("vietnam-disabled");
+        $(".vietnam, .vietnam-disabled").removeClass("vietnam");
+        $(".russian, .russian-disabled").addClass("russian-disabled");
+        $(".russian, .russian-disabled").removeClass("russian");
+})
+
+$(".vietnam-disabled, .vietnam").click(function() {
+    console.log("vietnam");
+    $(".greeting").text("Xin chào, mình là Lan.");
+    $("#statement").text("Nhà thiết kế & người vẽ tranh minh họa.");
+    $("#end-statement").text("Mình biết gồm ba thứ tiếng với nền học giáo giục và tâm lý.");
+    $(".english, .english-disabled").addClass("english-disabled");
+    $(".english, .english-disabled").removeClass("english");
+    $(this).addClass("vietnam");
+    $(this).removeClass("vietnam-disabled");
+    $(".russian, .russian-disabled").addClass("russian-disabled");
+    $(".russian, .russian-disabled").removeClass("russian");
+})
+
+$("#down").click(function(e) {
+    e.preventDefault();
+    var offset = 0;
+    var target = this.hash;
+    if ($(this).data('offset') != undefined) offset = $(this).data('offset');
+    $('html,body').stop().animate({
+        'scrollTop': $(target).offset().top - offset
+    },'slow', function() {});
 });
