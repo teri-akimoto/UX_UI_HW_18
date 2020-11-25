78de93db2b31abdde20e2fa45841e24ba16b4e63
teri-akimoto <nguyenhlan96@gmail.com>
2020-11-25 01:14:37 -0800

    Side Nav Overflow set up
    + RWD for case study layout

--- JavaScript/About.js
+++ JavaScript/About.js
@@ -1,24 +1,4 @@
-$("#summary-tab").click(function(e) {
-    e.preventDefault();
-    var offset = 0;
-    var target = this.hash;
-    if ($(this).data('offset') != undefined) offset = $(this).data('offset');
-    $('html,body').stop().animate({
-        'scrollTop': $(target).offset().top - offset
-    },'slow', function() {});
-});
-
-$("#ux-skills-tab").click(function(e) {
-    e.preventDefault();
-    var offset = 0;
-    var target = this.hash;
-    if ($(this).data('offset') != undefined) offset = $(this).data('offset');
-    $('html,body').stop().animate({
-        'scrollTop': $(target).offset().top - offset
-    },'slow', function() {});
-});
-
-$("#tool-set-tab").click(function(e) {
+$("#summary-tab, #ux-skills-tab, #tool-set-tab").click(function(e) {
     e.preventDefault();
     var offset = 0;
     var target = this.hash;
@@ -29,17 +9,19 @@ $("#tool-set-tab").click(function(e) {
 });
 
 $(".side-menu").click(function() {
-    if ($(".side-menu").height() > 20) {
-        console.log("shrink!");
-        $(this).animate({height: '20'}, 300);
-        $(".side-tab").hide(200);
-    }
+    if($(window).width() < 500) {
+        if ($(".side-menu").height() > 20) {
+            console.log("shrink!");
+            $(this).animate({height: '20'}, 300);
+            $(".side-tab").hide(200);
+        }
 
-    else if ($(".side-menu").height() == 20){
-        console.log('show');
-        var menuHeight = window.innerHeight / 6;
-        var resumeLoc = window.innerHeight / 6 + 70 + 20;
-        $(this).animate({height: menuHeight}, 300);
-        $(".side-tab").show(400);
+        else if ($(".side-menu").height() == 20){
+            console.log('show');
+            var menuHeight = window.innerHeight / 6;
+            var resumeLoc = window.innerHeight / 6 + 70 + 20;
+            $(this).animate({height: menuHeight}, 300);
+            $(".side-tab").show(400);
+        }
     }
 });
--- JavaScript/Endangered-Species.js
+++ JavaScript/Endangered-Species.js
@@ -1,54 +1,4 @@
-$("#summary-tab").click(function(e) {
-    e.preventDefault();
-    var offset = 0;
-    var target = this.hash;
-    if ($(this).data('offset') != undefined) offset = $(this).data('offset');
-    $('html,body').stop().animate({
-        'scrollTop': $(target).offset().top - offset
-    },'slow', function() {});
-});
-
-$("#ux-research-tab").click(function(e) {
-    e.preventDefault();
-    var offset = 0;
-    var target = this.hash;
-    if ($(this).data('offset') != undefined) offset = $(this).data('offset');
-    $('html,body').stop().animate({
-        'scrollTop': $(target).offset().top - offset
-    },'slow', function() {});
-});
-
-$("#definition-tab").click(function(e) {
-    e.preventDefault();
-    var offset = 0;
-    var target = this.hash;
-    if ($(this).data('offset') != undefined) offset = $(this).data('offset');
-    $('html,body').stop().animate({
-        'scrollTop': $(target).offset().top - offset
-    },'slow', function() {});
-});
-
-$("#prototyping-tab").click(function(e) {
-    e.preventDefault();
-    var offset = 0;
-    var target = this.hash;
-    if ($(this).data('offset') != undefined) offset = $(this).data('offset');
-    $('html,body').stop().animate({
-        'scrollTop': $(target).offset().top - offset
-    },'slow', function() {});
-});
-
-$("#final-tab").click(function(e) {
-    e.preventDefault();
-    var offset = 0;
-    var target = this.hash;
-    if ($(this).data('offset') != undefined) offset = $(this).data('offset');
-    $('html,body').stop().animate({
-        'scrollTop': $(target).offset().top - offset
-    },'slow', function() {});
-});
-
-$("#reflection-tab").click(function(e) {
+$("#summary-tab, #ux-research-tab, #definition-tab, #prototyping-tab, #final-tab, #reflection-tab").click(function(e) {
     e.preventDefault();
     var offset = 0;
     var target = this.hash;
@@ -59,17 +9,19 @@ $("#reflection-tab").click(function(e) {
 });
 
 $(".side-case-menu").click(function() {
-    if ($(".side-case-menu").height() > 20) {
-        console.log("shrink!");
-        $(this).animate({height: '20'}, 300);
-        $(".side-tab").hide(200);
-    }
+    if($(window).width() < 500) {  
+        if ($(".side-case-menu").height() > 20) {
+            console.log("shrink!");
+            $(this).animate({height: '20'}, 300);
+            $(".side-tab").hide(200);
+        }
 
-    else if ($(".side-case-menu").height() == 20){
-        console.log('show');
-        var menuHeight = window.innerHeight / 6;
-        var resumeLoc = window.innerHeight / 6 + 70 + 20;
-        $(this).animate({height: menuHeight}, 300);
-        $(".side-tab").show(400);
+        else if ($(".side-case-menu").height() == 20){
+            console.log('show');
+            var menuHeight = window.innerHeight / 6;
+            var resumeLoc = window.innerHeight / 6 + 70 + 20;
+            $(this).animate({height: menuHeight}, 300);
+            $(".side-tab").show(400);
+        }
     }
 });
